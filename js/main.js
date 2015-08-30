bcb016d97236d72dbb74840452e01a701223fb13
evcaldwell <evcaldwell@gmail.com>
2015-08-30 19:59:24 -0400

    added task complete list

--- js/main.js
+++ js/main.js
@@ -4,13 +4,18 @@ $(document).on('ready', function(){
 		console.log($("#task-title").val());
 		console.log($("#task-description").val());
 		console.log($("#task-date").val());
-		$("#task-list").append(
-			$("<li>" +  
-    		  	"<input type=\"checkbox\">" +
+		
+		var input = $("<input type=\"checkbox\">");
+		var li = $("<li>" +  
 	  			$("#task-title").val() +
 				$("#task-description").val() +
-				$("#task-date").val() + "</li>")
-		);	
+				$("#task-date").val() + "</li>");
+		li.prepend(input);	
+		$("#task-list").append(li);
+			
+		$(input).click(function(){
+		$("#task-complete").append(li);
+		});
 	});
 	
 		
