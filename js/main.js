20d66f2f063f5aaeffaa260c3ade72e9a7574253
evcaldwell <evcaldwell@gmail.com>
2015-08-30 19:27:29 -0400

    input fields + task list and checkbox working

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-
+$(document).on('ready', function(){
+//this
+	$("#add-button").click(function(){
+		console.log($("#task-title").val());
+		console.log($("#task-description").val());
+		console.log($("#task-date").val());
+		$("#task-list").append(
+			$("<li>" +  
+    		  	"<input type=\"checkbox\">" +
+	  			$("#task-title").val() +
+				$("#task-description").val() +
+				$("#task-date").val() + "</li>")
+		);	
+	});
+	
+		
+});
